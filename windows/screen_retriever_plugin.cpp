3d32846ed4e137cdf4bdcbd4a96ed207df6eb572
LiJianying <lijy91@foxmail.com>
2021-09-25 00:54:46 +0800

    [windows] Add getCursorScreenPoint, getPrimaryDisplay, getAllDisplays Methods

--- windows/screen_retriever_plugin.cpp
+++ windows/screen_retriever_plugin.cpp
@@ -12,41 +12,46 @@
 #include <memory>
 #include <sstream>
 
-namespace {
+const double kBaseDpi = 96.0;
+
+namespace
+{
     std::unique_ptr<flutter::MethodChannel<flutter::EncodableValue>, std::default_delete<flutter::MethodChannel<flutter::EncodableValue>>> channel = nullptr;
 
-    class ScreenRetrieverPlugin : public flutter::Plugin {
+    class ScreenRetrieverPlugin : public flutter::Plugin
+    {
     public:
-        static void RegisterWithRegistrar(flutter::PluginRegistrarWindows* registrar);
+        static void RegisterWithRegistrar(flutter::PluginRegistrarWindows *registrar);
 
         ScreenRetrieverPlugin();
 
         virtual ~ScreenRetrieverPlugin();
 
     private:
-        flutter::PluginRegistrarWindows* registrar;
+        flutter::PluginRegistrarWindows *registrar;
 
         void ScreenRetrieverPlugin::_EmitEvent(std::string eventName);
-        
+
         HWND ScreenRetrieverPlugin::GetMainWindow();
         void ScreenRetrieverPlugin::GetCursorScreenPoint(
-            const flutter::MethodCall<flutter::EncodableValue>& method_call,
+            const flutter::MethodCall<flutter::EncodableValue> &method_call,
             std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result);
         void ScreenRetrieverPlugin::GetPrimaryDisplay(
-            const flutter::MethodCall<flutter::EncodableValue>& method_call,
+            const flutter::MethodCall<flutter::EncodableValue> &method_call,
             std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result);
         void ScreenRetrieverPlugin::GetAllDisplays(
-            const flutter::MethodCall<flutter::EncodableValue>& method_call,
+            const flutter::MethodCall<flutter::EncodableValue> &method_call,
             std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result);
         // Called when a method is called on this plugin's channel from Dart.
         void HandleMethodCall(
-            const flutter::MethodCall<flutter::EncodableValue>& method_call,
+            const flutter::MethodCall<flutter::EncodableValue> &method_call,
             std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result);
     };
 
     // static
     void ScreenRetrieverPlugin::RegisterWithRegistrar(
-        flutter::PluginRegistrarWindows* registrar) {
+        flutter::PluginRegistrarWindows *registrar)
+    {
         channel =
             std::make_unique<flutter::MethodChannel<flutter::EncodableValue>>(
                 registrar->messenger(), "screen_retriever",
@@ -55,60 +60,126 @@ namespace {
         auto plugin = std::make_unique<ScreenRetrieverPlugin>();
 
         channel->SetMethodCallHandler(
-            [plugin_pointer = plugin.get()](const auto& call, auto result)
-        {
-            plugin_pointer->HandleMethodCall(call, std::move(result));
-        });
+            [plugin_pointer = plugin.get()](const auto &call, auto result)
+            {
+                plugin_pointer->HandleMethodCall(call, std::move(result));
+            });
 
         registrar->AddPlugin(std::move(plugin));
     }
 
+    ScreenRetrieverPlugin::ScreenRetrieverPlugin() {}
 
-    ScreenRetrieverPlugin::ScreenRetrieverPlugin() { }
+    ScreenRetrieverPlugin::~ScreenRetrieverPlugin() {}
 
-    ScreenRetrieverPlugin::~ScreenRetrieverPlugin() { }
-
-    void ScreenRetrieverPlugin::_EmitEvent(std::string eventName) {
+    void ScreenRetrieverPlugin::_EmitEvent(std::string eventName)
+    {
         flutter::EncodableMap args = flutter::EncodableMap();
-            args[flutter::EncodableValue("eventName")] = flutter::EncodableValue(eventName);
+        args[flutter::EncodableValue("eventName")] = flutter::EncodableValue(eventName);
         channel->InvokeMethod("onEvent", std::make_unique<flutter::EncodableValue>(args));
     }
 
-    HWND ScreenRetrieverPlugin::GetMainWindow() {
+    HWND ScreenRetrieverPlugin::GetMainWindow()
+    {
         return ::GetAncestor(registrar->GetView()->GetNativeWindow(), GA_ROOT);
     }
 
+    flutter::EncodableMap MonitorToEncodableMap(HMONITOR monitor)
+    {
+        MONITORINFO info;
+        info.cbSize = sizeof(MONITORINFO);
+        ::GetMonitorInfo(monitor, &info);
+        UINT dpi = FlutterDesktopGetDpiForMonitor(monitor);
+        double scale_factor = dpi / kBaseDpi;
+
+        flutter::EncodableMap size = flutter::EncodableMap();
+        size[flutter::EncodableValue("width")] = flutter::EncodableValue(static_cast<double>(info.rcMonitor.right));
+        size[flutter::EncodableValue("height")] = flutter::EncodableValue(static_cast<double>(info.rcMonitor.bottom));
+
+        flutter::EncodableMap display = flutter::EncodableMap();
+
+        display[flutter::EncodableValue("id")] = flutter::EncodableValue(0);
+        display[flutter::EncodableValue("name")] = flutter::EncodableValue("");
+        display[flutter::EncodableValue("size")] = flutter::EncodableValue(size);
+        display[flutter::EncodableValue("scaleFactor")] = flutter::EncodableValue(scale_factor);
+
+        return display;
+    }
+
+    BOOL CALLBACK MonitorRepresentationEnumProc(HMONITOR monitor, HDC hdc,
+                                                LPRECT clip, LPARAM list_ref)
+    {
+        flutter::EncodableValue *monitors = reinterpret_cast<flutter::EncodableValue *>(list_ref);
+        flutter::EncodableMap display = MonitorToEncodableMap(monitor);
+        std::get<flutter::EncodableList>(*monitors).push_back(display);
+        return TRUE;
+    }
+
     void ScreenRetrieverPlugin::GetCursorScreenPoint(
-        const flutter::MethodCall<flutter::EncodableValue>& method_call,
-        std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result) {
-        result->NotImplemented();
+        const flutter::MethodCall<flutter::EncodableValue> &method_call,
+        std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result)
+    {
+        const flutter::EncodableMap &args = std::get<flutter::EncodableMap>(*method_call.arguments());
+
+        double device_pixel_ratio = std::get<double>(args.at(flutter::EncodableValue("devicePixelRatio")));
+
+        double x, y;
+        POINT cursorPos;
+        GetCursorPos(&cursorPos);
+        x = cursorPos.x / device_pixel_ratio * 1.0f;
+        y = cursorPos.y / device_pixel_ratio * 1.0f;
+
+        flutter::EncodableMap result_data = flutter::EncodableMap();
+        result_data[flutter::EncodableValue("x")] = flutter::EncodableValue(x);
+        result_data[flutter::EncodableValue("y")] = flutter::EncodableValue(y);
+
+        result->Success(flutter::EncodableValue(result_data));
     }
 
     void ScreenRetrieverPlugin::GetPrimaryDisplay(
-        const flutter::MethodCall<flutter::EncodableValue>& method_call,
-        std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result) {
-        result->NotImplemented();
+        const flutter::MethodCall<flutter::EncodableValue> &method_call,
+        std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result)
+    {
+        POINT ptZero = {0, 0};
+        HMONITOR monitor = MonitorFromPoint(ptZero, MONITOR_DEFAULTTOPRIMARY);
+        flutter::EncodableMap display = MonitorToEncodableMap(monitor);
+        result->Success(flutter::EncodableValue(display));
     }
-    
+
     void ScreenRetrieverPlugin::GetAllDisplays(
-        const flutter::MethodCall<flutter::EncodableValue>& method_call,
-        std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result) {
-        result->NotImplemented();
+        const flutter::MethodCall<flutter::EncodableValue> &method_call,
+        std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result)
+    {
+
+        flutter::EncodableValue displays(std::in_place_type<flutter::EncodableList>);
+
+        ::EnumDisplayMonitors(nullptr, nullptr, MonitorRepresentationEnumProc,
+                              reinterpret_cast<LPARAM>(&displays));
+
+        flutter::EncodableMap result_data = flutter::EncodableMap();
+        result_data[flutter::EncodableValue("displays")] = displays;
+
+        result->Success(result_data);
     }
 
     void ScreenRetrieverPlugin::HandleMethodCall(
-        const flutter::MethodCall<flutter::EncodableValue>& method_call,
-        std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result) {
-        if (method_call.method_name().compare("getCursorScreenPoint") == 0) {
+        const flutter::MethodCall<flutter::EncodableValue> &method_call,
+        std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result)
+    {
+        if (method_call.method_name().compare("getCursorScreenPoint") == 0)
+        {
             GetCursorScreenPoint(method_call, std::move(result));
         }
-        else if (method_call.method_name().compare("getPrimaryDisplay") == 0) {
+        else if (method_call.method_name().compare("getPrimaryDisplay") == 0)
+        {
             GetPrimaryDisplay(method_call, std::move(result));
         }
-        else if (method_call.method_name().compare("getAllDisplays") == 0) {
+        else if (method_call.method_name().compare("getAllDisplays") == 0)
+        {
             GetAllDisplays(method_call, std::move(result));
         }
-        else {
+        else
+        {
             result->NotImplemented();
         }
     }
@@ -116,8 +187,9 @@ namespace {
 } // namespace
 
 void ScreenRetrieverPluginRegisterWithRegistrar(
-    FlutterDesktopPluginRegistrarRef registrar) {
+    FlutterDesktopPluginRegistrarRef registrar)
+{
     ScreenRetrieverPlugin::RegisterWithRegistrar(
         flutter::PluginRegistrarManager::GetInstance()
-        ->GetRegistrar<flutter::PluginRegistrarWindows>(registrar));
+            ->GetRegistrar<flutter::PluginRegistrarWindows>(registrar));
 }
