0e7e75f7b695b81abeadc4ad2d3a1beef64a1d71
LiJianying <lijy91@foxmail.com>
2022-05-08 14:05:07 +0800

    [linux] Add visiblePosition, visibleSize fields to Display model

--- linux/screen_retriever_plugin.cc
+++ linux/screen_retriever_plugin.cc
@@ -10,26 +10,25 @@
   (G_TYPE_CHECK_INSTANCE_CAST((obj), screen_retriever_plugin_get_type(), \
                               ScreenRetrieverPlugin))
 
-struct _ScreenRetrieverPlugin
-{
+struct _ScreenRetrieverPlugin {
   GObject parent_instance;
-  FlPluginRegistrar *registrar;
+  FlPluginRegistrar* registrar;
 };
 
-G_DEFINE_TYPE(ScreenRetrieverPlugin, screen_retriever_plugin, g_object_get_type())
+G_DEFINE_TYPE(ScreenRetrieverPlugin,
+              screen_retriever_plugin,
+              g_object_get_type())
 
 // Gets the window being controlled.
-GtkWindow *get_window(ScreenRetrieverPlugin *self)
-{
-  FlView *view = fl_plugin_registrar_get_view(self->registrar);
+GtkWindow* get_window(ScreenRetrieverPlugin* self) {
+  FlView* view = fl_plugin_registrar_get_view(self->registrar);
   if (view == nullptr)
     return nullptr;
 
   return GTK_WINDOW(gtk_widget_get_toplevel(GTK_WIDGET(view)));
 }
 
-FlValue *monitor_to_flvalue(GdkMonitor *monitor)
-{
+FlValue* monitor_to_flvalue(GdkMonitor* monitor) {
   GdkRectangle frame;
   gdk_monitor_get_geometry(monitor, &frame);
 
@@ -37,24 +36,44 @@ FlValue *monitor_to_flvalue(GdkMonitor *monitor)
   fl_value_set_string_take(size, "width", fl_value_new_float(frame.width));
   fl_value_set_string_take(size, "height", fl_value_new_float(frame.height));
 
-  const char *name = gdk_monitor_get_model(monitor);
+  GdkRectangle workarea_rect;
+  gdk_monitor_get_workarea(monitor, &workarea_rect);
+
+  // printf("x: %d y: %%d", workarea_rect.x, workarea_rect.y);
+
+  auto visible_size = fl_value_new_map();
+  fl_value_set_string_take(visible_size, "width",
+                           fl_value_new_float(workarea_rect.width));
+  fl_value_set_string_take(visible_size, "height",
+                           fl_value_new_float(workarea_rect.height));
+
+  auto visible_position = fl_value_new_map();
+  fl_value_set_string_take(visible_position, "x",
+                           fl_value_new_float(workarea_rect.x));
+  fl_value_set_string_take(visible_position, "y",
+                           fl_value_new_float(workarea_rect.y));
+
+  const char* name = gdk_monitor_get_model(monitor);
   gint scale_factor = gdk_monitor_get_scale_factor(monitor);
 
   g_autoptr(FlValue) value = fl_value_new_map();
   fl_value_set_string_take(value, "id", fl_value_new_float(0));
   fl_value_set_string_take(value, "name", fl_value_new_string(name));
   fl_value_set_take(value, fl_value_new_string("size"), size);
-  fl_value_set_string_take(value, "scaleFactor", fl_value_new_float(scale_factor));
+  fl_value_set_take(value, fl_value_new_string("visibleSize"), visible_size);
+  fl_value_set_take(value, fl_value_new_string("visiblePosition"),
+                    visible_position);
+  fl_value_set_string_take(value, "scaleFactor",
+                           fl_value_new_float(scale_factor));
 
   return fl_value_ref(value);
 }
 
-static FlMethodResponse *get_cursor_screen_point(ScreenRetrieverPlugin *self,
-                                                 FlValue *args)
-{
-  GdkDisplay *display = gdk_display_get_default();
-  GdkSeat *seat = gdk_display_get_default_seat(display);
-  GdkDevice *pointer = gdk_seat_get_pointer(seat);
+static FlMethodResponse* get_cursor_screen_point(ScreenRetrieverPlugin* self,
+                                                 FlValue* args) {
+  GdkDisplay* display = gdk_display_get_default();
+  GdkSeat* seat = gdk_display_get_default_seat(display);
+  GdkDevice* pointer = gdk_seat_get_pointer(seat);
 
   int x, y;
   gdk_device_get_position(pointer, NULL, &x, &y);
@@ -66,28 +85,24 @@ static FlMethodResponse *get_cursor_screen_point(ScreenRetrieverPlugin *self,
   return FL_METHOD_RESPONSE(fl_method_success_response_new(result_data));
 }
 
-static FlMethodResponse *get_primary_display(ScreenRetrieverPlugin *self,
-                                             FlValue *args)
-{
-  GdkDisplay *display = gdk_display_get_default();
-  GdkMonitor *monitor = gdk_display_get_primary_monitor(display);
+static FlMethodResponse* get_primary_display(ScreenRetrieverPlugin* self,
+                                             FlValue* args) {
+  GdkDisplay* display = gdk_display_get_default();
+  GdkMonitor* monitor = gdk_display_get_primary_monitor(display);
 
   g_autoptr(FlValue) result_data = monitor_to_flvalue(monitor);
 
   return FL_METHOD_RESPONSE(fl_method_success_response_new(result_data));
 }
 
-static FlMethodResponse *get_all_displays(ScreenRetrieverPlugin *self,
-                                          FlValue *args)
-{
-
+static FlMethodResponse* get_all_displays(ScreenRetrieverPlugin* self,
+                                          FlValue* args) {
   auto displays = fl_value_new_list();
 
-  GdkDisplay *display = gdk_display_get_default();
+  GdkDisplay* display = gdk_display_get_default();
   gint n_monitors = gdk_display_get_n_monitors(display);
-  for (gint i = 0; i < n_monitors; i++)
-  {
-    GdkMonitor *monitor = gdk_display_get_monitor(display, i);
+  for (gint i = 0; i < n_monitors; i++) {
+    GdkMonitor* monitor = gdk_display_get_monitor(display, i);
     fl_value_append_take(displays, monitor_to_flvalue(monitor));
   }
 
@@ -99,55 +114,46 @@ static FlMethodResponse *get_all_displays(ScreenRetrieverPlugin *self,
 
 // Called when a method call is received from Flutter.
 static void screen_retriever_plugin_handle_method_call(
-    ScreenRetrieverPlugin *self,
-    FlMethodCall *method_call)
-{
+    ScreenRetrieverPlugin* self,
+    FlMethodCall* method_call) {
   g_autoptr(FlMethodResponse) response = nullptr;
 
-  const gchar *method = fl_method_call_get_name(method_call);
-  FlValue *args = fl_method_call_get_args(method_call);
-  if (strcmp(method, "getCursorScreenPoint") == 0)
-  {
+  const gchar* method = fl_method_call_get_name(method_call);
+  FlValue* args = fl_method_call_get_args(method_call);
+  if (strcmp(method, "getCursorScreenPoint") == 0) {
     response = get_cursor_screen_point(self, args);
-  }
-  else if (strcmp(method, "getPrimaryDisplay") == 0)
-  {
+  } else if (strcmp(method, "getPrimaryDisplay") == 0) {
     response = get_primary_display(self, args);
-  }
-  else if (strcmp(method, "getAllDisplays") == 0)
-  {
+  } else if (strcmp(method, "getAllDisplays") == 0) {
     response = get_all_displays(self, args);
-  }
-  else
-  {
+  } else {
     response = FL_METHOD_RESPONSE(fl_method_not_implemented_response_new());
   }
 
   fl_method_call_respond(method_call, response, nullptr);
 }
 
-static void screen_retriever_plugin_dispose(GObject *object)
-{
+static void screen_retriever_plugin_dispose(GObject* object) {
   G_OBJECT_CLASS(screen_retriever_plugin_parent_class)->dispose(object);
 }
 
-static void screen_retriever_plugin_class_init(ScreenRetrieverPluginClass *klass)
-{
+static void screen_retriever_plugin_class_init(
+    ScreenRetrieverPluginClass* klass) {
   G_OBJECT_CLASS(klass)->dispose = screen_retriever_plugin_dispose;
 }
 
-static void screen_retriever_plugin_init(ScreenRetrieverPlugin *self) {}
+static void screen_retriever_plugin_init(ScreenRetrieverPlugin* self) {}
 
-static void method_call_cb(FlMethodChannel *channel, FlMethodCall *method_call,
-                           gpointer user_data)
-{
-  ScreenRetrieverPlugin *plugin = SCREEN_RETRIEVER_PLUGIN(user_data);
+static void method_call_cb(FlMethodChannel* channel,
+                           FlMethodCall* method_call,
+                           gpointer user_data) {
+  ScreenRetrieverPlugin* plugin = SCREEN_RETRIEVER_PLUGIN(user_data);
   screen_retriever_plugin_handle_method_call(plugin, method_call);
 }
 
-void screen_retriever_plugin_register_with_registrar(FlPluginRegistrar *registrar)
-{
-  ScreenRetrieverPlugin *plugin = SCREEN_RETRIEVER_PLUGIN(
+void screen_retriever_plugin_register_with_registrar(
+    FlPluginRegistrar* registrar) {
+  ScreenRetrieverPlugin* plugin = SCREEN_RETRIEVER_PLUGIN(
       g_object_new(screen_retriever_plugin_get_type(), nullptr));
 
   plugin->registrar = FL_PLUGIN_REGISTRAR(g_object_ref(registrar));
@@ -155,11 +161,9 @@ void screen_retriever_plugin_register_with_registrar(FlPluginRegistrar *registra
   g_autoptr(FlStandardMethodCodec) codec = fl_standard_method_codec_new();
   g_autoptr(FlMethodChannel) channel =
       fl_method_channel_new(fl_plugin_registrar_get_messenger(registrar),
-                            "screen_retriever",
-                            FL_METHOD_CODEC(codec));
-  fl_method_channel_set_method_call_handler(channel, method_call_cb,
-                                            g_object_ref(plugin),
-                                            g_object_unref);
+                            "screen_retriever", FL_METHOD_CODEC(codec));
+  fl_method_channel_set_method_call_handler(
+      channel, method_call_cb, g_object_ref(plugin), g_object_unref);
 
   g_object_unref(plugin);
 }
